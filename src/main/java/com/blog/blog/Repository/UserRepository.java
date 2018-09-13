13b31e366f035a017172bbdac07a218b37d57a5a
Don Pho <donpho@Dons-MacBook-Air.local>
2018-09-13 09:07:23 -0500

    relationship exercise

---
+++ src/main/java/com/blog/blog/Repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.blog.blog.Repository;
+
+import com.blog.blog.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
